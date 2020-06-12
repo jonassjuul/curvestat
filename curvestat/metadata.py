bdacdcfd40f462bccf99a2d22b866745925d04b7
Jonas S. Juul <31031606+jonassjuul@users.noreply.github.com>
2020-06-12 10:29:55 +0200

    Update metadata.py

--- curvestat/metadata.py
+++ curvestat/metadata.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Contains information about this package.
+"""
 
+__version__ = "0.0.1"
+
+__author__ = "In alphabetical order: Jonas L. Juul"
+__copyright__ = "Copyright 2020, In alphabetical order: Jonas L. Juul"
+__credits__ = ["In alphabetical order: Jonas L. Juul"]
+__license__ = "MIT"
+__maintainer__ = "Jonas L. Juul"
+__email__ = "jlju@dtu.dk"
+__status__ = "Development"
